96e1994c243af0f857f967a48b69628b1d6745a9
rbarsk <rbarsk@gmail.com>
2024-10-04 14:38:08 +0100

    V.02 - Green

--- Integral_v01/ContentView.swift
+++ Integral_v01/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "2.circle")
+                .resizable()
+                .scaledToFit()
+                
+            Text("Version 2.0")
+                .font(.largeTitle)
+                .fontWeight(.bold)
         }
         .padding()
+        .foregroundColor(.green)
     }
 }
 
